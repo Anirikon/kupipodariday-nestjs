9d934c3a6ce16082ab3d00a03898d461c229273a
Kirill Beltchikov <bkm.front@yandex.ru>
2025-09-17 22:25:45 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module( {
+    controllers: [AppController],
+    providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'student',
+            password: 'root',
+            database: 'kupiproday-nest',
+            autoLoadEntities: true,
+            entities: [],
+            synchronize: true,
+        })
+    ]
+})
+export class AppModule {}
