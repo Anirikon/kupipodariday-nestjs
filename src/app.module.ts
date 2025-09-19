7c655a9733a6bcca1c71a30f4b69219b84fa48ad
Anirikon <anirikon21@gmail.com>
2025-09-19 20:13:34 +0300

    Генерация каркаса API, настрока конфига и библиотек

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,32 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module( {
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`}),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'student',
-            password: 'root',
-            database: 'kupiproday-nest',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: String(process.env.POSTGRES_PASSWORD),
+            database: String(process.env.POSTGRES_DB),
             autoLoadEntities: true,
             entities: [],
             synchronize: true,
-        })
+        }),
+        UsersModule,
+        WishesModule,
+        WishlistsModule,
+        OffersModule,
     ]
 })
 export class AppModule {}
