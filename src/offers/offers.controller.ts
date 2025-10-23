0e7beed6db34c29559bb390aa49e5b35ac0aabf8
Anirikon <anirikon21@gmail.com>
2025-10-23 16:09:57 +0300

    Фикс контроллера и сервиса offer

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { OffersService } from './offers.service';
 import { CreateOfferDto } from './dtoOffers/create-offer.dto';
+import { JwtGuard } from 'src/auth/jwt.guard';
 
+@UseGuards(JwtGuard)
 @Controller('offers')
 export class OffersController {
     constructor(private offerService: OffersService) { }
     @Post()
-    create(@Body() dto: CreateOfferDto) {
-        return this.offerService.create(dto)
+    create(@Body() dto: CreateOfferDto, @Req() req) {
+        return this.offerService.create(req.user.id, dto)
     }
 
     @Get()
--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,12 +1,20 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Offer } from './offer.model';
 import { Repository } from 'typeorm';
 import { CreateOfferDto } from './dtoOffers/create-offer.dto';
+import { User } from 'src/users/user.model';
+import { Wish } from 'src/wishes/wish.model';
+import { DataSource } from 'typeorm/browser';
 
 @Injectable()
 export class OffersService {
-    constructor(@InjectRepository(Offer) private offerRepository: Repository<Offer>) {}
+    constructor(
+        @InjectRepository(Offer) private offerRepository: Repository<Offer>,
+        @InjectRepository(User) private userRepository: Repository<User>,
+        @InjectRepository(Wish) private wishRepository: Repository<Wish>,
+        private dataSource: DataSource,
+) {}
         async findAll(): Promise<Offer[]> {
             return this.offerRepository.find();
         }
@@ -19,7 +27,52 @@ export class OffersService {
             return offer;
         }
 
-        async create(offer: CreateOfferDto): Promise<CreateOfferDto> {
-            return this.offerRepository.save(offer)
-        }
+        async create(id: number, offer: CreateOfferDto): Promise<CreateOfferDto> {
+            const { amount, hidden, itemId } = offer
+            const user = await this.userRepository.findOneBy( {id: id} )
+            const wish = await this.wishRepository.findOne({ where: {id: itemId}, relations: ['owner'] })
+            
+            if(!user) {
+                throw new NotFoundException('Пользователь не найден')
+            }
+
+            if(!wish) {
+                throw new NotFoundException('Подарков нет')
+            }
+
+            if (user.id === wish.owner.id) {
+                throw new BadRequestException('Нельзя скидываться на свой подарок');
+             }
+
+            if (wish.raised + amount > wish.price) {
+                throw new BadRequestException('Превышение лимита суммы подарка')
+            }
+
+            const queryRunner = this.dataSource.createQueryRunner();
+            await queryRunner.connect();
+            await queryRunner.startTransaction();
+
+            try {
+                wish.raised += amount
+            const newOffer = queryRunner.manager.create(Offer, {
+      amount,
+      hidden,
+      user,
+      item: wish
+    });
+        const savedOffer = await queryRunner.manager.save(newOffer);
+            await queryRunner.manager.save(wish)
+    await queryRunner.commitTransaction();
+    return {
+      itemId: savedOffer.item.id,
+      amount: savedOffer.amount,
+      hidden: savedOffer.hidden
+    };
+  } catch (err) {
+    await queryRunner.rollbackTransaction();
+    throw new InternalServerErrorException('Не удалось создать предложение');
+  } finally {
+    await queryRunner.release();
+  }
+}
 }
